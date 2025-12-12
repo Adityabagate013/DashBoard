a2a710f52ace01f798a2b67baf51b1567b26f58b
Adityabagate013 <bagateaditya1@gmail.com>
2025-12-12 16:21:30 +0530

    Adding AllTask & CreateTask file.

--- src/component/Dashboard/AdminDashboard.jsx
+++ src/component/Dashboard/AdminDashboard.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
 import Header from '../../other/Header'
+import CreateTask from '../../other/CreateTask'
+import AllTask from '../../other/AllTask'
 
 const AdminDashboard = () => {
   return (
-    <div className='h-screen w-full p-10'>
+    <div className='h-screen w-full p-7'>
       <Header />
-      <div>
-        <form action="">
-          <h3>Task Title</h3>
-          <input type="text" placeholder='Make a UI Design' />
-          <h3>Description</h3>
-          <textarea></textarea>
-          <h3>Date</h3>
-          <input type="date" name="" id="" />
-          <input type="text"  />
-        </form>
-      </div>
+      <CreateTask />
+      <AllTask />
 
     </div>
   )
